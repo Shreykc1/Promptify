fe0a81cd6b5bc41224b8fbbb380fbe82e007501e
shreykc1 <Admin@MUM15115CPU1211.SVKMGRP.COM>
2024-08-30 10:02:11 +0530

    lander

--- app/page.js
+++ app/page.js
@@ -1,8 +1,15 @@
+import { TextHoverEffect } from '@/components/ui/text-hover'
 import React from 'react'
 
 const Home = () => {
   return (
-    <div>Home</div>
+    <div className='w-full h-screen z-20 flex-center text-[#FDFDFD] font-neue'>
+        <div className='flex-center flex-col gap-4'>
+            {/* <h1 className='font-bold text-6xl'>Promptify</h1> */}
+            <TextHoverEffect text="Promptify"/>
+            <p className=''>Discovering and sharing prompts made easy with promptify.</p>
+        </div>
+    </div>
   )
 }
 
